c498682ef46f820e5da43b63d444ee45fd673ed6
KiingX14 <kiingx1409@gmail.com>
2025-05-04 00:16:48 +1000

    Initial Commit

---
+++ FitnessAppTests/FitnessAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppTests.swift
+//  FitnessAppTests
+//
+//  Created by Le Hoang Thanh Lam on 4/5/2025.
+//
+
+import Testing
+@testable import FitnessApp
+
+struct FitnessAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
